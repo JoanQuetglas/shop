fa3e739fc0788ed6b5acafb600bdc577e2147470
JoanLikes <JoanLikes@192.168.7.178>
2015-03-03 19:53:50 +0100

    Constructors de Pagament

--- Shop/src/classe/Pagament.java
+++ Shop/src/classe/Pagament.java
@@ -6,6 +6,21 @@ public class Pagament {
 	private TipusPagaments tipus;
 	private String ppCompta;
 	private int ppContrasenya;
+	private String visaPropietari;
 	private int visaCodi;
+	private int visaNumTargeta
+	private int visaDataExpiracio;
+	
+	public Pagament(int visaCodi, int visaDataExpiracio , int visaNumTargeta , String visaPropietari, TipusPagaments tipus ) {
+		this.visaCodi = visaCodi;
+		this.visaDataExpiracio = visaDataExpiracio;
+		this.visaNumTargeta = visaNumTargeta;
+		this.visaPropietari = visaPropietari;
+		this.tipus = tipus;
+	}
+	
+	public Pagament(TipusPagaments tipus){
+		this.tipus = tipus;
+	}
 	
 }
